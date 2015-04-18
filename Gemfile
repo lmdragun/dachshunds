e5ee66ffe6101cd7f591f534969143d533a3f181
Lindsey <lmdragun@gmail.com>
2015-04-18 13:21:20 -0400

    experimenting with colors

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
 gem 'httparty'
-gem 'colourlovers'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.1'
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -8,8 +8,7 @@ class WelcomeController < ApplicationController
 
 		@response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=dachshund&api_key=dc6zaTOxFJmzC&limit=116").parsed_response["data"][@random]["images"]["original"]["url"]
 
-		cl = Colourlovers::Client.new
-		@color = cl.random_color["hex"]
+		@color = "%06x" % (rand * 0xffffff)
 	end
 
 end
