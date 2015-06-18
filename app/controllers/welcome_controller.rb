8a646d7dd1e4850199a22feec572137c9c8269ce
Lindsey <lmdragun@gmail.com>
2015-06-18 16:38:05 -0400

    changed amt for giphy

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -4,7 +4,7 @@ class WelcomeController < ApplicationController
 
 	def index
 
-		@random = rand(110).to_i
+		@random = rand(120).to_i
 
 		@response = HTTParty.get("http://api.giphy.com/v1/gifs/search?q=dachshund&api_key=dc6zaTOxFJmzC&limit=116").parsed_response["data"][@random]["images"]["original"]["url"]
 
